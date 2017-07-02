d630af01931f42b26ebb1fa62419737a612cca24
glp <glpayson@gmail.com>
2017-07-02 17:20:29 -0400

    Changing project name. Fixing unbalanced paren).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cbauth "0.1.0"
+(defproject bcauth "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
